90590cdab8ca06cdc35c93e08f16f1f07860dff7
Scott <scott.heron@outlook.com>
2016-01-26 23:03:20 -0800

    final

---
+++ script/script.js
@@ -0,0 +1,37 @@
+
+  $(document).ready(function(){
+  	
+  	var $pushtolocal = [];
+  	//var $pulledfromlocal = [];
+  	  	
+  	//$('ul').append('<li><input type="checkbox" class="checkedbox"></input><span>' + $pulledfromlocal + '</span></li>');
+  	
+  	
+  	$('#deleteButton').on('click', function(e){
+  		e.preventDefault(); 
+  		
+  		$("li :checked").parent().remove();
+  		localStorage.removeItem('userinput');
+  		
+	});
+
+  	$('#todo-field').on('submit', function(e){
+  		e.preventDefault();
+  		
+  		localStorage.setItem ('userinput', $('#todo-field input').val()); 		
+  		var $todo = localStorage.getItem('userinput');
+  		$('#todo-field input').val('');
+  		console.log(localStorage.getItem('userinput'));
+
+  		if ($todo != '') {
+  			$('ul').append('<li><input type="checkbox" class="checkedbox"></input><span>' + $todo + '</span></li>');
+  			$pushtolocal.push($todo);
+  			localStorage.setItem ('userinput', $pushtolocal);
+
+  		}
+  		//console.log(pushtolocal);
+  		
+  	});
+
+  	  
+  });
